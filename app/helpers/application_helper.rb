a20ccf7d4b4a1ec04df491c324792b9f86d35987
Georgi Martsenkov <georgi@Georgis-MacBook-Pro.local>
2014-08-20 20:04:02 +0300

    Participation validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	
+ def participate(event)
+		
+  event.participants.each do |participant|
+   if participant.user.id == current_user.id
+	return false
+   else
+	return true
+   end
+  end
+ end
+ 
 end
