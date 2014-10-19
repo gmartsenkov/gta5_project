03a80260a4df5b6427405cd3dc059a92ee9600df
Georgi Martsenkov <garmev@gmail.com>
2014-10-20 01:44:31 +0300

    Redesign

---
+++ app/assets/javascripts/categories.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$(window).on('scroll', function(){
+
+	if($(window).scrollTop() < 10){
+
+		$('.magic-border').removeClass("magic-border-magnificent");
+	}
+	else
+	{ 
+		$('.magic-border').addClass('magic-border-magnificent');
+	}
+
+	});
+});
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @events = Event.all
   end
 
   # GET /categories/1
