67deb56fe506480e73612fc91956865b0321f4e1
Georgi Martsenkov <georgi@Georgis-MacBook-Pro.local>
2014-11-27 12:15:32 +0000

    api concatenate pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,25 @@
-class HomeController < ApplicationController
+ class HomeController < ApplicationController
   def index
   end
 
   def api
     @q = params[:q]
-
+    @data = []
     resp = Net::HTTP.get_response(URI.parse('https://api.parkatmyhouse.com/1.1/location/?q='+ @q))
-    @data = resp.body
+    @data[0] = resp.body
+    total = resp['x-total-result-count'].to_i
+    per = resp['x-result-count'].to_i
+    if (total / per) < 3
+    pages = (total / per )
+    else
+      pages = 3
+    end
+    a = 0
+    for i in 1..pages
+    a += 1
+      resp = Net::HTTP.get_response(URI.parse('https://api.parkatmyhouse.com/1.1/location/?q='+ @q + '?page=' + a.to_s))
+      @data[a] = resp.body
+    end
 
     render json: @data, :callback => params['callback']
   end
